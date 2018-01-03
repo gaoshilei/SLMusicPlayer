e4d59b47808a3f615c2148acc4f0267034bcccb9
gaoshilei <731351288@qq.com>
2018-01-03 15:20:16 +0800

    Initial Commit

---
+++ SLMusicPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SLMusicPlayer
+//
+//  Created by gaoshilei on 2018/1/3.
+//  Copyright © 2018年 gaoshilei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SLMusicPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SLMusicPlayer
+//
+//  Created by gaoshilei on 2018/1/3.
+//  Copyright © 2018年 gaoshilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
